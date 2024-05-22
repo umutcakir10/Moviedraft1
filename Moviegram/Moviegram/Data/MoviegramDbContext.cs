9efa5519bc804e3f72a16495a766b2761d1fb464
umutcakir10 <ucakir81@gmail.com>
2024-05-22 16:56:34 +0300

    movie yarı ilk draft

---
+++ Moviegram/Moviegram/Data/MoviegramDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moviegram.Models.Domain;
+
+namespace Moviegram.Data
+{
+	public class MoviegramDbContext: DbContext
+	{
+		public MoviegramDbContext(DbContextOptions options) : base(options) 
+		{
+		}
+
+        public DbSet<MoviePost> MoviePosts { get; set; }
+
+		public DbSet<Tag> Tags { get; set; }
+	}
+}
---
+++ Moviegram/Moviegram/Repositories/IMoviePostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Moviegram.Models.Domain;
+
+namespace Moviegram.Repositories
+{
+    public interface IMoviePostRepository
+    {
+        Task<IEnumerable<MoviePost>> GetAllAsync();
+
+        Task<MoviePost?> GetAsync(Guid id);
+
+        Task<MoviePost> AddAsync(MoviePost moviePost);
+
+        Task<MoviePost?> UpdateAsync(MoviePost moviePost);
+
+        Task<MoviePost?> DeleteAsync(Guid id);
+    }
+}
